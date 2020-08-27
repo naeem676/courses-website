7220435f343560566cb1ba5a95b35ca98bdaa34e
naeem uddin <ali.naeem939@gmail.com>
2020-08-27 14:47:14 +0600

    summary cart added

--- src/compunents/Online.js
+++ src/compunents/Online.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import fakeData from '../fakedata';
 import Advetise from './Advetise/Advetise';
+import './Online.css';
 
 const Online = () => {  
     const coures = fakeData;
     return (
-        <div>
-         <ul>
+        <div className="online">
+        <div className="coures-container">
+        <ul>
             {
-                coures.map( data => <Advetise courses={data}></Advetise>)
+                coures.map( data => <Advetise courses={data} key={data.email}></Advetise>)
             }
         
          </ul>
+
+        </div>
+        <div className="cart-container">
+            <h3>Order summary:</h3>
+        </div>
+        
+        
         </div>
     );
 };
